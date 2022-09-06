11460fa293e95729909a347217ee4fe2662bbc74
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2022-09-06 18:48:12 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (33.36%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.java
+++ 814-binary-tree-pruning/814-binary-tree-pruning.java
@@ -16,11 +16,8 @@
 class Solution {
     public TreeNode pruneTree(TreeNode root) {
         if(root == null) return null;
-        TreeNode lsa = pruneTree(root.left);
-        TreeNode rsa = pruneTree(root.right);
-        root.left = lsa;
-        root.right = rsa;
-        if(lsa == null && rsa == null && root.val == 0) return null;
-        return root;
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        return (root.left == null && root.right == null && root.val == 0) ? null : root;
     }
 }
