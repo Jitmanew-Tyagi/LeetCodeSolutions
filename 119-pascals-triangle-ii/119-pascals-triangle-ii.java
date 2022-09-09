404ff17d9efaf8ac0eb41c46d28d9a997f66ad7f
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2022-09-09 15:53:42 +0530

    Time: 2 ms (59.53%), Space: 41.6 MB (70.05%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -1,17 +1,10 @@
 class Solution {
     public List<Integer> getRow(int n) {
-        int[][] ans = new int[2][n + 1];
-        ans[0][0] = 1;
-        for(int j = 0; j < n; j ++) {
-            for(int i = 0; i <= n; i ++) {
-                ans[1][i] = i == 0 ? ans[0][i] : ans[0][i - 1] + ans[0][i];
-            }
-            ans[0] = ans[1];
-            ans[1] = new int[ans[1].length];
-        }
-        List<Integer> fans = new ArrayList<>();
-        for(int i : ans[0]) fans.add(i);
-        return fans;
-        
+        Integer[] ans = new Integer[n + 1];
+        Arrays.fill(ans, 0);
+        ans[0] = 1;
+        for(int i = 1; i <= n; i ++) 
+            for(int j = i; j > 0; j --) ans[j] = ans[j] + ans[j - 1];
+        return Arrays.asList(ans);   
     }
 }
