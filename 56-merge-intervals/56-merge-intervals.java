cd26994125843aa218ab878040565d004cc79c50
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2022-09-13 21:26:19 +0530

    Time: 23 ms (10.14%), Space: 55.5 MB (34.05%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,20 +1,44 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-        Stack<int[]> st = new Stack<>();
-        for(int[] oned : intervals) {
-            if(st.isEmpty()) st.push(oned);
+        Dt[] ans = new Dt[intervals.length];
+        for (int i = 0; i < intervals.length; i++) {
+            ans[i] = new Dt(intervals[i][0], intervals[i][1]);
+        }
+        Arrays.sort(ans);
+        Stack<Dt> st = new Stack<>();
+        for (int i = 0; i < intervals.length; i++) {
+            Dt temp = new Dt(ans[i].f, ans[i].s);
+            if (st.isEmpty()) st.push(temp);
             else {
-                if(oned[0] <= st.peek()[1]) {
-                    int[] pop = st.pop();
-                    st.push(new int[] {pop[0], Math.max(pop[1], oned[1])});
-                } else {
-                    st.push(oned);
+                if (temp.f > st.peek().s) st.push(temp);
+                else {
+                    Dt t = st.pop();
+                    st.push(new Dt(t.f, Math.max(t.s, temp.s)));
                 }
             }
+            
         }
-        int ans[][] = new int[st.size()][2], idx = 0;
-        for(int[] oned : st) ans[idx ++] = oned;
-        return ans;
+        int[][] ans_ = new int[st.size()][2];
+        int i = st.size() - 1;
+        while(!st.isEmpty()) {
+            Dt x = st.pop();
+            int[] x_ = {x.f, x.s};
+            ans_[i --] = x_;
+        }
+        
+        return ans_;
     }
+   
+    public static class Dt implements Comparable<Dt> {
+        int f, s;
+        Dt(int f, int s) {
+            this.f = f;
+            this.s = s;
+        }
+
+        public int compareTo (Dt other) {
+            if (this.f != other.f) return (this.f - other.f);
+            return (this.s - other.s);
+        } 
+    } 
 }
