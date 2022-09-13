365d44425874a58b5043367f06639724026df585
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2022-09-13 20:21:24 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (96.45%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
@@ -5,8 +5,9 @@ class Solution {
             if(i > m1) {
                 m2 = m1;
                 m1 = i;
+            } else if(i > m2) {
+                m2 = i;
             }
-            else if(i > m2) m2 = i;
         }
         return (m1 - 1) * (m2 - 1);
     }
