d3b1d68e9a6c44c47ae97ff2d0a72e5c87a17f9e
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2022-11-13 14:13:30 +0530

    Time: 1 ms (99.54%), Space: 41.9 MB (88.39%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int compress(char[] chars) {
+        int i = 0, count = 1;
+        StringBuilder sb = new StringBuilder();
+        while(i < chars.length) {
+            if(i != chars.length - 1 && chars[i] == chars[i + 1]) count ++;
+            else {
+                sb.append(chars[i]).append(count == 1 ? "" : count);
+                count = 1;
+            }
+            i ++;
+        }
+        int idx = 0;
+        for(char c : sb.toString().toCharArray()) {
+            chars[idx ++] = c;
+        }
+        return sb.length();
+    }
+}
