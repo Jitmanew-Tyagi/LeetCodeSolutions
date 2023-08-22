e147137a838a557499e3390fa2d4d6ad5b3632f4
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-08-22 23:24:45 +0530

    Time: 1 ms (99.39%), Space: 43.6 MB (95.17%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,7 +1,11 @@
 class Solution {
-    public int pivotIndex(int[] nums) {
-        int n = nums.length;
-        for(int i = 1; i < n; i ++) nums[i] += nums[i - 1];
+    public int pivotIndex(int[] arr) {
+        int n = arr.length, sum = 0;
+        int[] nums = new int[n];
+        for(int i = 0; i < n; i ++) {
+            sum += arr[i];
+            nums[i] = sum;
+        }
         if(nums[n - 1] - nums[0] == 0) return 0;
         for(int i = 1; i < n; i ++) if(nums[i - 1] == nums[n - 1] - nums[i]) return i;
         if(n > 1 && nums[n - 2] == 0) return n - 1;
