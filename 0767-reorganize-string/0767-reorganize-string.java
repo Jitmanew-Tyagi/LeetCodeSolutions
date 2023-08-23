275c4d6af4980b3e7d010d965aa463969f9ad6a8
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-08-24 01:42:33 +0530

    Time: 1 ms (87.48%), Space: 40.5 MB (87.10%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -2,25 +2,26 @@ class Solution {
     public String reorganizeString(String s) {
         int[] fm = new int[26];
         for(char c : s.toCharArray()) fm[c -'a'] += 1;
-        Queue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
-        for(int i = 0; i < 26; i ++) if(fm[i] > 0) pq.add(new int[]{i, fm[i]});
-        StringBuilder sb = new StringBuilder();
-        while(!pq.isEmpty()){
-            int[] out = pq.poll();
-            char c = (char)(out[0] + 'a');
-            if(sb.isEmpty() || sb.charAt(sb.length() - 1) != c) {
-                sb.append(c);
-                if(-- out[1] > 0) pq.add(out);
-            } else {
-                if(pq.isEmpty()) return "";
-                int[] sout = pq.poll();
-                char sc = (char)(sout[0] + 'a');
-                sb.append(sc);
-                if(-- sout[1] > 0) pq.add(sout);
-                pq.add(out);
+        int max = 0, idx = 0;
+        for(int i = 0; i < 26; i ++) {
+            if(fm[i] > fm[max]) max = i;
+        }
+        if(fm[max] > (s.length() + 1) / 2) return "";
+        char[] ans = new char[s.length()];
+        while(--fm[max] >= 0) {
+            ans[idx] = (char)(max + 'a');
+            idx += 2;
+        }
+        for(int i = 0; i < 26; i ++) {
+            if(fm[i] == 0) continue;
+            while(-- fm[i] >= 0){
+                if(idx >= ans.length) idx = 1;
+                ans[idx] = (char)('a' + i);
+                idx += 2;
             }
         }
-        System.gc();
-        return sb.toString();
+        StringBuilder str = new StringBuilder();
+        for(char c : ans) str.append(c);
+        return str.toString();
     }
 }
