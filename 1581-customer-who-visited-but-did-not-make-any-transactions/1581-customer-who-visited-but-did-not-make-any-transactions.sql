72df80338463708d008cd8d55317504540f0bbe2
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-09-05 23:30:00 +0530

    Time: 4483 ms (5.21%), Space: 0B (100.00%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,21 +1,14 @@
-WITH t1 AS(
-    SELECT 
-        v.customer_id AS customer_id,
-        t.transaction_id AS transaction_id
-    FROM
-        Visits as v
-    LEFT JOIN 
-        Transactions as t
-    ON
-        v.visit_id = t.visit_id
-    WHERE
-        t.transaction_id IS NULL
-)
 
-SELECT
-    customer_id,
+SELECT 
+    v.customer_id,
     COUNT(*) AS count_no_trans
-FROM 
-    t1
-GROUP BY
+FROM
+    Visits as v
+LEFT JOIN 
+    Transactions as t
+ON
+    v.visit_id = t.visit_id
+WHERE
+    t.transaction_id IS NULL
+GROUP BY 
     customer_id;
