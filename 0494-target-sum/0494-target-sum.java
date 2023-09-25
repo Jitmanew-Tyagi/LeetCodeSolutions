e66c77c04fbac06ba0ca1b756c696ef42b9af80c
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-09-25 16:32:00 +0530

    Time: 9 ms (64.98%), Space: 43.6 MB (28.34%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,14 +1,14 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int[][] dp = new int[21][2001];
-        for(int[] d : dp) Arrays.fill(d, -1);
+        for(int[] d : dp) Arrays.fill(d, -1001);
         return rec(nums.length, nums, 0, target, 0, dp);
     }
     
     public int rec(int n, int[] nums, int i, int target, int sum, int[][] dp) {
         if(i == n) 
             return dp[i][sum + 1000] = sum == target ? 1 : 0;
-        if(dp[i][sum + 1000] != -1) return dp[i][sum + 1000];
+        if(dp[i][sum + 1000] != -1001) return dp[i][sum + 1000];
         int ans = 0;
         ans += rec(n, nums, i + 1, target, sum - nums[i], dp);
         ans += rec(n, nums, i + 1, target, sum + nums[i], dp);
