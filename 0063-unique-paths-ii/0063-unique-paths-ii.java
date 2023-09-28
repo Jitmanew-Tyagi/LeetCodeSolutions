c11697bf1723d796cbb4e4440233de134a3a9f3f
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-09-28 13:38:59 +0530

    Time: 2 ms (8.78%), Space: 40.4 MB (80.45%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,5 +1,7 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[obstacleGrid.length - 1][obstacleGrid[0].length - 1] == 1) 
+            return 0;
         int[][] dirns = {{1, 0}, {0, 1}};
         int[][] dp = new int[obstacleGrid.length + 1][obstacleGrid[0].length + 1];
         for(int[] d : dp) Arrays.fill(d, -1);
@@ -7,14 +9,20 @@ class Solution {
     }
     
     public int rec(int[][] maze, int i, int j, int[][] dirns, int[][] dp) {
-        if(i >= maze.length || j >= maze[0].length || maze[i][j] == 1) 
-            return dp[i][j] = 0;
-        if(i == maze.length - 1 && j == maze[0].length - 1) 
-            return dp[i][j] = 1;
-        if(dp[i][j] != -1) return dp[i][j];
-        int count = 0;
-        for(int[] dir : dirns) 
-            count += rec(maze, i + dir[0], j + dir[1], dirns, dp);
-        return dp[i][j] = count;
+       for(i = maze.length - 1; i >= 0; i --) {
+           for(j = maze[0].length; j >= 0; j --) {
+               
+               if(i == maze.length - 1 && j == maze[0].length - 1) {
+                     dp[i][j] = 1;
+                   continue;
+               }
+               int count = 0;
+               for(int[] dir : dirns) 
+                    if(i + dir[0] < maze.length && j + dir[1] < maze[0].length && maze[i + dir[0]][j + dir[1]] == 0)
+                        count += dp[i + dir[0]][j + dir[1]];
+               dp[i][j] = count;
+           }
+       }
+       return dp[0][0];        
     }
 }
