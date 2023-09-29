76652b9aeb68261e33269933aff3600d886d6382
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-09-29 18:28:03 +0530

    Time: 1 ms (100.00%), Space: 43 MB (90.92%) - LeetHub

--- 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.java
+++ 2859-sum-of-values-at-indices-with-k-set-bits/2859-sum-of-values-at-indices-with-k-set-bits.java
@@ -1,19 +1,11 @@
 class Solution {
     public int sumIndicesWithKSetBits(List<Integer> nums, int k) {
-        int ans = 0;
-        for(int i = 0; i < nums.size(); i ++) 
-            if(countSetBits(i) == k) 
-                ans += nums.get(i);
-        return ans;       
-    }
-    
-    public int countSetBits(int n) {
-        int ans = 0;
-        for(int i = 0; i < 32; i ++) if(isSet(n, i)) ans ++;
-        return ans;
-    }
-    
-    public boolean isSet(int n, int k) {
-        return ((n & (1 << k)) > 0);
+        int sum=0;
+        for(int i=0; i<nums.size();i++){
+            if(Integer.bitCount(i)==k){
+                sum+=nums.get(i);
+            }
+        }
+        return sum;
     }
 }
