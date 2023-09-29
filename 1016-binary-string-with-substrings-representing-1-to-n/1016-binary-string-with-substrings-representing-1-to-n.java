acc19e10e4bda01d030f554b4071c3c96f84ea8b
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-09-29 11:33:23 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (41.36%) - LeetHub

--- 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.java
+++ 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.java
@@ -1,19 +1,8 @@
 class Solution {
-    public boolean queryString(String s, int n) {
-        int k = n;
-        Queue<String> que = new LinkedList<>();
-        List<String> ans = new ArrayList<>();
-        que.add("1");
-        while(!que.isEmpty()) {
-            int size = que.size();
-            while(size --> 0) {
-                String out = que.remove();
-                if(!s.contains(out)) return false;
-                if(--k == 0) return true;
-                que.add(out + "0");
-                que.add(out + "1");
-            }
-        }
+    public boolean queryString(String S, int N) {
+        for (int i = N; i > N / 2; --i)
+            if (!S.contains(Integer.toBinaryString(i)))
+                return false;
         return true;
     }
 }
