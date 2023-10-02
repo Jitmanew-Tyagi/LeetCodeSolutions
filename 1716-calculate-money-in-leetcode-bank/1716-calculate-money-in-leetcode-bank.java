9d534bdfc7c1a277025d7d45257b772daf41321b
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-10-02 23:34:43 +0530

    Time: 1 ms (66.80%), Space: 39.3 MB (26.84%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int totalMoney(int n) {
+        int start = 1, ans = 0;
+        while(n > 0) {
+            for(int i = 0; i < 7 && n > 0; i ++) {
+                ans += (start + i);
+                n --;
+            }
+            start ++;
+        }
+        return ans;
+    }
+}
