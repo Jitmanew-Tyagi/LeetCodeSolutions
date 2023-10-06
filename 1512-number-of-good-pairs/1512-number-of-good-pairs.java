10337edb811a15a332cfeba5565ba189dc7122c0
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-10-06 21:36:45 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (90.03%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -1,10 +1,8 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
         int hm[] = new int[101], ans = 0;
-        for(int ele : nums) {
-            ans += hm[ele];
-            hm[ele] ++;
-        }
+        for(int ele : nums) hm[ele] ++;
+        for(int ele : hm) ans += (ele * (ele - 1)) / 2;
         return ans;
     }
 }
