e4b19a47ab6baa1e04dfd1e0d3262f157e889f1f
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-10-06 09:30:20 +0530

    Time: 1 ms (61.46%), Space: 38.9 MB (74.61%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -3,7 +3,7 @@ class Solution {
         if(n == 2) return 1;
         if(n == 3) return 2;
         int[] dp = new int[n + 1];
-        return rec(n, dp);
+        return tab(n, dp);
     }
     
     public int rec(int n, int[] dp){
@@ -14,5 +14,19 @@ class Solution {
             ans = Math.max(ans, rec(n - i, dp) * i);
         }
         return dp[n] = ans;
+    }
+    
+    public int tab(int N, int[] dp) {
+        for(int n = 0; n <= N; n ++) {
+            if(n <= 1) {
+                dp[n] = 1;
+                continue;
+            } 
+            int ans = 1;
+            for(int i = 1; i <= n; i ++) 
+                ans = Math.max(ans, dp[n - i] * i);
+            dp[n] = ans;
+        }
+        return dp[N];
     }
 }
