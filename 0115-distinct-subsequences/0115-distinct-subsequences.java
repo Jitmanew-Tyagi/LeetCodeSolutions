9ed048fdda3dab39bb798da5523fdb434376a4fd
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-10-06 12:27:03 +0530

    Time: 21 ms (7.97%), Space: 46.6 MB (76.02%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -3,7 +3,8 @@ class Solution {
         int[][] dp = new int[s.length() + 1][t.length() + 1];
         for(int[] d : dp) Arrays.fill(d, -1);
         // return rec(s, t);
-        return memo(s, t, 0, 0, dp);
+        // return memo(s, t, 0, 0, dp);
+        return tab(s, t, 0, 0, dp);
     }
     
     public int rec(String s, String t) {
@@ -27,5 +28,29 @@ class Solution {
         }
         ans += memo(s, t, i + 1, j, dp);
         return dp[i][j] = ans;
+    }
+    
+    public int tab(String s, String t, int I, int J, int[][] dp) {
+        for(int i = s.length(); i >= I; i --) {
+            for(int j = t.length(); j >= J; j --) {
+                if(t.length() == j) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                
+                if(s.length() == i) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                
+                int ans = 0;
+                if(s.charAt(i) == t.charAt(j)) {
+                    ans += dp[i + 1][j + 1];//memo(s, t, i + 1, j + 1, dp);
+                }
+                ans += dp[i + 1][j];//memo(s, t, i + 1, j, dp);
+                dp[i][j] = ans;
+            }
+        }
+        return dp[I][J];
     }
 }
