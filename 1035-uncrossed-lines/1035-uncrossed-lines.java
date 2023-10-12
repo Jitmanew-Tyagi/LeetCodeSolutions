9430aa421557ab1dbddd6b896ee7107d0dd0d805
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-10-12 13:49:17 +0530

    Time: 7 ms (24.84%), Space: 43.2 MB (33.77%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.java
+++ 1035-uncrossed-lines/1035-uncrossed-lines.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
         int[][] dp = new int[nums1.length + 1][nums2.length + 1];
         for(int[] d : dp) Arrays.fill(d, -1);
-        return memo(nums1, nums2, 0, 0, dp);
+        return tab(nums1, nums2, 0, 0, dp);
     }
     
     public int rec(int[] a, int[] b, int i, int j) {
@@ -16,5 +16,22 @@ class Solution {
         if(dp[i][j] != -1) return dp[i][j];
         if(a[i] == b[j]) return dp[i][j] = 1 + memo(a, b, i + 1, j + 1, dp);
         return dp[i][j] = Math.max(memo(a, b, i + 1, j, dp), memo(a, b, i, j + 1, dp));
+    }
+    
+    public int tab(int[] a, int[] b, int I, int J, int[][] dp) {
+        for(int i = a.length; i >= I; i --) {
+            for(int j = b.length; j >= J; j --) {
+                if(i == a.length || j == b.length) {
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if(a[i] == b[j]) {
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                    continue;
+                }
+                dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+            }
+        }
+        return dp[I][J];
     }
 }
