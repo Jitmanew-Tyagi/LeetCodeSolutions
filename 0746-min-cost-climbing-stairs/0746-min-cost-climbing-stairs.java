bb86684d3f5d8d0d21e0db0476e1fc245a0bda59
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-10-13 09:43:28 +0530

    Time: 1 ms (46.22%), Space: 42.6 MB (86.05%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,6 +1,8 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        return Math.min(rec(cost, cost.length - 1, new int[cost.length]), rec(cost, cost.length - 2, new int[cost.length]));
+        // return Math.min(rec(cost, cost.length - 1, new int[cost.length]), rec(cost, cost.length - 2, new int[cost.length]));
+        return Math.min(tab(cost, cost.length - 1, new int[cost.length]), tab(cost, cost.length - 2, new int[cost.length]));
+    
     }
     
     public int rec(int[] arr, int n, int[]dp) {
@@ -8,5 +10,15 @@ class Solution {
         if(dp[n] != 0) return dp[n];
         if(n <= 1) return dp[n] = arr[n];
         return dp[n] = Math.min(rec(arr, n - 1, dp), rec(arr, n - 2, dp)) + arr[n];
+    }
+    
+    public int tab(int[] arr, int N, int[] dp) {
+        for(int n = 0; n <= N; n ++) {
+            if(n <= 1) dp[n] = arr[n];
+            else {
+                dp[n] = Math.min(dp[n - 1], n - 2 >= 0 ? dp[n - 2] : Integer.MAX_VALUE) + arr[n];
+            }
+        }
+        return dp[N];
     }
 }
