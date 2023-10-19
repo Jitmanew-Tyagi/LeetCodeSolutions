f3f247b36d90a8b8498668134f814a0e9ab5ff69
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-10-19 13:06:34 +0530

    Time: 5 ms (64.31%), Space: 43.4 MB (39.29%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -3,9 +3,10 @@ class Solution {
         HashSet<String> hs = new HashSet<>();
         for(String str : wordDict) hs.add(str);
         // return rec(s, 0, 0, hs);
-        int[][] dp = new int[s.length() + 1][s.length() + 1];
-        for(int[] d : dp) Arrays.fill(d, -1);
+        int[] dp = new int[s.length() + 1];
+        Arrays.fill(dp, -1);
         return memo(s, 0, 0, hs, dp);
+        // return tab(s, 0, 0, hs, new boolean[s.length() + 1][s.length() + 1]);
     }
     
     public boolean rec(String s, int si, int ei, HashSet<String> hs) {
@@ -16,16 +17,36 @@ class Solution {
         return ans;
     }
     
-    public boolean memo(String s, int si, int ei, HashSet<String> hs, int[][] dp) {
+    public boolean memo(String s, int si, int ei, HashSet<String> hs, int[] dp) {
         if(ei == s.length()) {
-            dp[si][ei] = si == s.length() ? 1 : 0;
-            return dp[si][ei] == 1;
+            dp[si] = si == s.length() ? 1 : 0;
+            return dp[si] == 1;
         }
-        if(dp[si][ei] != -1) return dp[si][ei] == 1;
+        if(dp[si] != -1) return dp[si] == 1;
         boolean ans = false;
         if(hs.contains(s.substring(si, ei + 1))) ans = ans || memo(s, ei + 1, ei + 1, hs, dp);
         ans = ans || memo(s, si, ei + 1, hs, dp);
-        dp[si][ei] = ans ? 1 : 0;
+        dp[si] = ans ? 1 : 0;
         return ans;
+    }
+    
+    public boolean tab(String s, int SI, int EI, HashSet<String> hs, boolean[][] dp) {
+        for(int si = s.length(); si >= SI; si --) {
+            for(int ei = s.length(); ei >= EI; ei --) {
+                boolean ans = false;
+                if(ei == s.length()) {
+                    dp[si][ei] = si == s.length();
+                    continue;
+                }
+                if(ei <= si) {
+                    dp[si][ei] = true;
+                    continue;
+                }
+                if(hs.contains(s.substring(si, ei + 1))) ans = ans || dp[ei + 1][ei + 1];
+                ans = ans || dp[si][ei + 1];
+                dp[si][ei] = ans;
+            }
+        }
+        return dp[SI][EI];
     }
 }
