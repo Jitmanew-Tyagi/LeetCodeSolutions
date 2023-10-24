1a7fd3b38b4990e040c018197131c7a945943c28
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-10-24 23:36:25 +0530

    Time: 16 ms (93.47%), Space: 43.1 MB (38.83%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
@@ -1,40 +1,27 @@
 class Solution {
     public int findNumberOfLIS(int[] nums) {
-        int n = nums.length, dp[] = new int[n], ans = 0;
-	    int len = tab(nums, dp);
-	    // for(int l : dp) {
-	    // System.out.print(l + " ");
-	    // if(l == len) ans ++;
-	    // }
-	    // return ans;
-        return tab(nums, dp);
-    }
-    
-    public int tab(int[] nums, int[] dp) {
-        int maxLen = 0, lengths[] = new int[dp.length], maxLenIdx = 0;
+        int n = nums.length, dp[] = new int[n], lengths[] = new int[n];
         Arrays.fill(lengths, 1);
-        for(int i = 0; i < nums.length; i ++) {
-            int ans = 1;
+        int maxLen = 0, maxCount = 0;
+        for(int i = 0; i < n; i ++) {
+            int len = 1;
             for(int j = i - 1; j >= 0; j --) {
-                if(nums[j] < nums[i]) {
-                    int newLength = dp[j] + 1;
-                    if(newLength == ans) lengths[i] += lengths[j];
-                    else if(newLength > ans) {
-                        ans = newLength;
-                        lengths[i] = lengths[j];
-                    }
+                if(nums[j] >= nums[i]) continue;
+                int newLen = dp[j] + 1;
+                if(newLen > len) {
+                    len = newLen;
+                    lengths[i] = lengths[j];
+                } else if(newLen == len) {
+                    lengths[i] += lengths[j];
                 }
             }
-            dp[i] = ans;
-            if(ans > maxLen) {
-                maxLen = ans;
-                maxLenIdx = i;
-            }
+            dp[i] = len;
+            maxLen = Math.max(dp[i], maxLen);
         }
-        int maxLenEq = 0, temp = 0;
-        for(int i = 0; i < dp.length; i ++) {
-            if(dp[i] == maxLen) temp += lengths[i];
-        }
-        return temp;
+        for(int i = 0; i < n; i ++) 
+            if(dp[i] == maxLen) maxCount += lengths[i];
+        return maxCount;  
     }
+    
+    
 }
