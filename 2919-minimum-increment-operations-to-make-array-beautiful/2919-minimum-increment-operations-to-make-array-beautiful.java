fe74726e253b10ed978dd2407cc33f9f54385e97
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-10-29 15:04:44 +0530

    Time: 5 ms (100.00%), Space: 60.9 MB (33.33%) - LeetHub

--- 2919-minimum-increment-operations-to-make-array-beautiful/2919-minimum-increment-operations-to-make-array-beautiful.java
+++ 2919-minimum-increment-operations-to-make-array-beautiful/2919-minimum-increment-operations-to-make-array-beautiful.java
@@ -2,9 +2,10 @@ class Solution {
     public long minIncrementOperations(int[] nums, int k) {
         long[] dp = new long[nums.length + 1];
         Arrays.fill(dp, -1);
-        return Math.min(climb(nums, 0, k, dp), 
-                        Math.min(climb(nums, 1, k, dp), 
-                                 climb(nums, 2, k, dp)));
+        // return Math.min(climb(nums, 0, k, dp), 
+        //                 Math.min(climb(nums, 1, k, dp), 
+        //                          climb(nums, 2, k, dp)));
+        return tab(nums, 0, k, dp);
     }
     
     public long climb(int[] nums, int i, int k, long[] dp) {
@@ -18,5 +19,22 @@ class Solution {
             }
         }
         return dp[i] = ans + cost;
+    }
+    
+    public long tab(int[] nums, int I, int k, long[] dp) {
+        for(int i = nums.length; i >= I; i --) {
+            if(i == nums.length) dp[i] = 0; 
+            else {
+                long ans = Long.MAX_VALUE;
+                long cost = Math.max(0, k - nums[i]);
+                for(int j = 1; j <= 3; j ++) {
+                    if(i + j <= nums.length) {
+                        ans = Math.min(ans, dp[i + j]);
+                    }
+                }
+                dp[i] = ans + cost;
+            }
+        }
+        return Math.min(dp[0], Math.min(dp[1], dp[2]));
     }
 }
