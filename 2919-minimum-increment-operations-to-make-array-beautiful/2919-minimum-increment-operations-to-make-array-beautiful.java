a20e8c84cf7bdc3039b2b38137d890fa2fbde02b
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-10-29 15:09:10 +0530

    Time: 3 ms (100.00%), Space: 60.3 MB (66.67%) - LeetHub

--- 2919-minimum-increment-operations-to-make-array-beautiful/2919-minimum-increment-operations-to-make-array-beautiful.java
+++ 2919-minimum-increment-operations-to-make-array-beautiful/2919-minimum-increment-operations-to-make-array-beautiful.java
@@ -1,7 +1,8 @@
 class Solution {
     public long minIncrementOperations(int[] nums, int k) {
-        long[] dp = new long[nums.length + 1];
-        return tab(nums, 0, k, dp);
+        // long[] dp = new long[nums.length + 1];
+        // return tab(nums, 0, k, dp);
+        return dpOptimised(nums, k);
     }
     
     public long tab(int[] nums, int I, int k, long[] dp) {
@@ -19,5 +20,16 @@ class Solution {
             }
         }
         return Math.min(dp[0], Math.min(dp[1], dp[2]));
+    }
+    
+    public long dpOptimised(int[] nums, int k) {
+        long dp, dp1 = 0, dp2 = 0, dp3 = 0; 
+        for(int ele : nums) {
+            dp = Math.max(0, k - ele) + Math.min(dp1, Math.min(dp2, dp3));
+            dp1 = dp2;
+            dp2 = dp3;
+            dp3 = dp;
+        }
+        return Math.min(dp1, Math.min(dp2, dp3));
     }
 }
