430518bffb6ad976ed065414e88eacf726be6b79
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-10-29 09:31:28 +0530

    Time: 3 ms (100.00%), Space: 43.7 MB (25.00%) - LeetHub

---
+++ 2917-find-the-k-or-of-an-array/2917-find-the-k-or-of-an-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findKOr(int[] nums, int k) {
+        int kor = 0;
+        for(int i = 0; i < 32; i ++) {
+            int count = 0;
+            for(int ele : nums) {
+                if(checkIfOn(ele, i)) count ++;
+            }
+            if(count >= k) kor += (int)Math.pow(2, i);
+        }
+        return kor;
+    }
+    
+    public boolean checkIfOn(int num, int k) {
+        int mask = 1 << k;
+        return (num & mask) > 0;
+    }
+}
