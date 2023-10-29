2edd1317b3bc6d45943dfa1f0a173ef407240e3e
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-10-29 22:46:45 +0530

    Time: 21 ms (36.24%), Space: 80.7 MB (89.55%) - LeetHub

---
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean containsCycle(char[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        boolean[][] vis = new boolean[n][m];
+        for(int i = 0; i < n; i ++) {
+            for(int j = 0; j < m; j ++) {
+                if(!vis[i][j]) if(consume(grid, i, j, n, m, vis)) return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean consume(char[][] grid, int i, int j, int n, int m, boolean[][] vis) {
+        Queue<Integer> que = new LinkedList<>();
+        que.add(i * m + j);
+        int[][] dirn = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+        while(!que.isEmpty()) {
+            int out = que.remove();
+            int ti = out / m, tj = out % m;
+            if(vis[ti][tj]) return true;
+            vis[ti][tj] = true;
+            for(int[] dir : dirn) {
+                int x = ti + dir[0], y = tj + dir[1];
+                if(x >= 0 && y >= 0 && x < n && y < m && grid[x][y] == grid[ti][tj] && !vis[x][y]) 
+                    que.add(x * m + y);
+            }
+        }
+        return false;
+    }
+}
