187099bd2aef98f9bd25b6a0a834b7130c0bde49
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-10-29 09:35:17 +0530

    Time: 3 ms (100.00%), Space: 43.2 MB (25.00%) - LeetHub

--- 2917-find-the-k-or-of-an-array/2917-find-the-k-or-of-an-array.java
+++ 2917-find-the-k-or-of-an-array/2917-find-the-k-or-of-an-array.java
@@ -1,18 +1,17 @@
 class Solution {
     public int findKOr(int[] nums, int k) {
         int kor = 0;
-        for(int i = 0; i < 32; i ++) {
+        for(int i = 0; i < 32; i ++) { //checking for all 32 bits
             int count = 0;
-            for(int ele : nums) {
-                if(checkIfOn(ele, i)) count ++;
+            for(int ele : nums) { // checking every element
+                if(checkIfOn(ele, i)) count ++; // if the bit is on. increase the count
             }
-            if(count >= k) kor = (kor | (1 << i));
+            if(count >= k) kor = (kor | (1 << i)); // if number of elements >= k, switch the bit ON
         }
         return kor;
     }
     
     public boolean checkIfOn(int num, int k) {
-        int mask = 1 << k;
-        return (num & mask) > 0;
+        return (num & (1 << k)) > 0;
     }
 }
