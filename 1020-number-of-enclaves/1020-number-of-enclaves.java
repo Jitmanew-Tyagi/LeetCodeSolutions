fafd224f6d8785c33404283ee7b3fa76b9cd5227
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-10-30 17:31:53 +0530

    Time: 7 ms (85.35%), Space: 56.3 MB (29.94%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -10,7 +10,7 @@ class Solution {
         
         for(int i = 1; i < n - 1; i ++) {
             for(int j = 1; j < m - 1; j ++) {
-                if(grid[i][j] == 1) ans += cnc(grid, i, j);
+                if(grid[i][j] == 1) ans ++;
             }
         }
         return ans;
