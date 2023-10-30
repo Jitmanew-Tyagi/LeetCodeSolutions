c218c68d1d9797662bb0ddcdf1c411e723daf007
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-10-30 17:00:22 +0530

    Time: 9 ms (54.13%), Space: 55.1 MB (42.90%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,10 +1,10 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
         int n = grid.length, m = grid[0].length;
-        for(int i = 0; i < n; i ++) cnc(grid, i, 0);
-        for(int i = 0; i < n; i ++) cnc(grid, i, m - 1);
-        for(int i = 0; i < m; i ++) cnc(grid, 0, i);
-        for(int i = 0; i < m; i ++) cnc(grid, n - 1, i);
+        for(int i = 0; i < n; i ++) consume(grid, i, 0);
+        for(int i = 0; i < n; i ++) consume(grid, i, m - 1);
+        for(int i = 0; i < m; i ++) consume(grid, 0, i);
+        for(int i = 0; i < m; i ++) consume(grid, n - 1, i);
         
         int ans = 0;
         
