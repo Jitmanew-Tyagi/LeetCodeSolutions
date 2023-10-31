ca6ba7e401940dae8055e05229ebb7100b09c99d
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-10-31 18:54:53 +0530

    Time: 3 ms (99.03%), Space: 44.9 MB (30.36%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -3,15 +3,21 @@ class Solution {
         List<Integer>[] graph = new ArrayList[n];
         for(int i = 0; i < n; i ++) graph[i] = new ArrayList<>();
         for(int[] a : pre) graph[a[0]].add(a[1]);
-        Queue<Integer> que = new LinkedList<>();
-        int idx = 0, ans[] = new int[n], ind[] = new int[n];
-        for(int[] a : pre) ind[a[1]] ++;
-        for(int i = 0; i < n; i ++) if(ind[i] == 0) que.add(i);
-        while(!que.isEmpty()) {
-            int out = que.remove();
-            ans[(n - 1) - (idx ++)] = out;
-            for(int nbr : graph[out]) if(--ind[nbr] == 0) que.add(nbr);
+        int idx = 0, ans[] = new int[n], vis[] = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i < n; i ++) if(vis[i] == 0) if(!DFS(graph, i, vis, st)) return new int[0];
+        while(!st.isEmpty()) ans[n - 1 - (idx ++)] = st.pop();
+        return ans;
+    }
+    
+    public boolean DFS(List<Integer>[] graph, int s, int[] vis, Stack<Integer> st) {
+        vis[s] = 1;
+        for(int nbr : graph[s]) {
+            if(vis[nbr] == 1) return false;
+            if(vis[nbr] == 0 && DFS(graph, nbr, vis, st) == false) return false;
         }
-        return idx == n ? ans : new int[0];
+        st.push(s);
+        vis[s] = 2;
+        return true;
     }
 }
