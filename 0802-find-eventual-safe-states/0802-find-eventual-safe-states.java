b4d4c33b90aa8f5430406fa5e5bdf167b7d9cd40
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2023-11-01 12:43:27 +0530

    Time: 24 ms (36.84%), Space: 55 MB (39.15%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,21 +1,24 @@
 class Solution {
-    public List<Integer> eventualSafeNodes(int[][] graph) {
-        int[] vis = new int[graph.length];
-        List<Integer> ans = new ArrayList<>();
-        for(int i = 0; i < graph.length; i ++) {
-            if(vis[i] == 0) DFS(i, graph, vis);
+    public List<Integer> eventualSafeNodes(int[][] g) {
+        int n = g.length, vis[] = new int[n], ind[] = new int[n];
+        List<Integer> graph[] = new ArrayList[n], ans = new ArrayList<>();
+        for(int i = 0; i < n; i ++) graph[i] = new ArrayList<>();
+        for(int i = 0; i < n; i ++) {
+            for(int ele : g[i]) {
+                graph[ele].add(i);
+                ind[i] ++;
+            }
         }
-        for(int i = 0; i < graph.length; i ++) if(vis[i] == 2) ans.add(i);
+        Queue<Integer> que = new LinkedList<>();
+        for(int i = 0; i < n; i ++) if(ind[i] == 0) que.add(i);
+        while(!que.isEmpty()) {
+            int out = que.remove();
+            ans.add(out);
+            for(int nbr : graph[out]) {
+                if(-- ind[nbr] == 0) que.add(nbr);
+            }
+        }
+        Collections.sort(ans);
         return ans;
-    }
-    
-    public boolean DFS(int s, int[][] graph, int[] vis) {
-        vis[s] = 1;
-        for(int nbr : graph[s]) {
-            if(vis[nbr] == 1) return true;
-            if(vis[nbr] == 0) if(DFS(nbr, graph, vis)) return true;
-        }
-        vis[s] = 2;
-        return false;
     }
 }
