7b69cd0dae7c8e8f33f8563645037ca58bb66c23
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-01-15 10:40:27 +0530

    Time: 1 ms (77.37%), Space: 45.5 MB (43.14%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -2,20 +2,36 @@ class Solution {
     public int trap(int[] height) {
         int n = height.length;
         int totalWater = 0;
+        int[] left = getLB(height);
+        int[] right = getRB(height);
         for(int bar = 0; bar < n; bar ++) {
             int bh = height[bar];
-            int lb = 0, rb = 0;
-            for(int i = bar - 1; i >= 0; i --) {
-                if(lb < height[i]) lb = height[i];
-            }
-            
-            for(int i = bar + 1; i < n; i ++) {
-                if(rb < height[i]) rb = height[i];
-            }
-            
+            int lb = left[bar], rb = right[bar];
             int water = Math.min(lb, rb) - bh;
             if(water > 0) totalWater += water;
         }
         return totalWater;
+    }
+    
+    public int[] getLB(int[] bars) {
+        int n = bars.length;
+        int[] ans = new int[n];
+        int lb = 0;
+        for(int i = 0; i < n; i ++) {
+            ans[i] = lb;
+            if(bars[i] > lb) lb = bars[i];
+        }
+        return ans;
+    }
+    
+    public int[] getRB(int[] bars) {
+        int n = bars.length;
+        int[] ans = new int[n];
+        int rb = 0;
+        for(int i = n - 1; i >= 0; i --) {
+            ans[i] = rb;
+            if(bars[i] > rb) rb = bars[i];
+        }
+        return ans;
     }
 }
