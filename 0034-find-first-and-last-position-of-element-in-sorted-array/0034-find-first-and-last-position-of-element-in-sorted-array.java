ec5439ae7b8ec52f6aed0f4721a3f61d589d9baf
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-01-16 15:00:10 +0530

    Time: 0 ms (100.00%), Space: 45.7 MB (35.65%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,23 +1,22 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        return new int[] {binarySearchMulti(nums, target, true), 
-                         binarySearchMulti(nums, target, false)};
+        int first = binarySearch(nums, target, true);
+        int last = binarySearch(nums, target, false);
+        return new int[] {first, last};
     }
     
-    public int binarySearchMulti(int[] arr, int ele, boolean focc) {
+    public int binarySearch(int[] arr, int x, boolean firstOcc) {
         int s = 0, e = arr.length - 1;
         int ans = -1;
         while(s <= e) {
-            int mid = (s + e) / 2; //better
-            if(arr[mid] == ele)  {
+            int mid = s + (e - s) / 2;
+            if(arr[mid] ==x) {
                 ans = mid;
-                if(focc == true) e = mid - 1;
+                if(firstOcc) e = mid - 1;
                 else s = mid + 1;
-            }
-            else if(arr[mid] <  ele) s = mid + 1;
-            else e = mid - 1;
+            } else if(arr[mid] < x)  s = mid + 1;
+             else  e = mid - 1;
         }
-
         return ans;
     }
 }
