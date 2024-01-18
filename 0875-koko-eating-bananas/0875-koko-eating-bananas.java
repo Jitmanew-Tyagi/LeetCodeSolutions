29f02118f9f55f6c2b51bcee324840e97150fb7e
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-01-18 10:17:18 +0530

    Time: 26 ms (44.73%), Space: 44.8 MB (57.85%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,25 +1,24 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int s = 1, e = maxOfArray(piles);
+        int min = 1, max = maxOfArray(piles);
         int ans = -1;
-        while(s <= e) {
-            int mid = s + (e - s) / 2;
-            if(isPossible(piles, mid, h)){
+        while(min <= max) {
+            int mid = min + (max - min) / 2;
+            if(isPossible(piles, mid, h)) {
                 ans = mid;
-                e = mid - 1;
-            } else s = mid + 1;
+                max = mid - 1;
+            } else min = mid + 1;
         }
         return ans;
     }
     
-    public boolean isPossible(int[] piles, int speed, int totalHours) {
-        int hours = 0;
-        for(int b : piles) {
-            hours += Math.ceil(b / (speed * 1.0));
+    public boolean isPossible(int[] piles, int speed, int hours) {
+        int time = 0;
+        for(int pile : piles) {
+            time += Math.ceil(pile / (speed * 1.0));
         }
-        return hours <= totalHours;
+        return time <= hours;
     }
-    
     
     public int maxOfArray(int[] arr) {
         int max = arr[0];
