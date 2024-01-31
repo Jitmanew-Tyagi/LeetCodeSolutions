8568e992e40029910333464093c987e2f080bcab
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-01-31 15:53:46 +0530

    Time: 7 ms (65.12%), Space: 44.6 MB (58.85%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,35 +9,10 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        List<TreeNode> ntrpForP = ntrp(root, p);
-        List<TreeNode> ntrpForQ = ntrp(root, q);
-        
-        int i = ntrpForP.size() - 1;
-        int j = ntrpForQ.size() - 1;
-        
-        while(i >= 0 && j >= 0 && ntrpForP.get(i) == ntrpForQ.get(j)) {
-            i --;
-            j --;
-        }
-        return ntrpForP.get(++ i);
-    }
-    
-    public List<TreeNode> ntrp(TreeNode root, TreeNode node) {
-        // BASE CASE\
-        if(root == null || root.val == node.val) {
-            List<TreeNode> base = new ArrayList<>();
-            if(root != null) base.add(root);
-            return base;
-        }
-        List<TreeNode> left = ntrp(root.left, node);
-        List<TreeNode> right = ntrp(root.right, node);
-        if(!left.isEmpty()) {
-            left.add(root);
-            return left;
-        } else if(!right.isEmpty()) {
-            right.add(root);
-            return right;
-        }
-        return new ArrayList<>();
+        if(root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left != null && right != null) return root;
+        return left != null ? left : right;
     }
 }
