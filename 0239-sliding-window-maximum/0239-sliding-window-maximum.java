247d1d4b3a25e8b082a1cb5f2525944aacea97da
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-02-03 12:07:31 +0530

    Time: 102 ms (10.62%), Space: 61.2 MB (26.70%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -2,11 +2,11 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length, ngr[] = ngr(nums);
         int[] ans = new int[n - k + 1];
-        int ng = 0;
+        int jmpr = 0;
         for(int i = 0; i <= n - k; i ++) {
-            if(ng < i) ng = i;
-            while(ngr[ng] < i + k) ng = ngr[ng];
-            ans[i] = nums[ng];
+            if(jmpr < i) jmpr = i;
+            while(ngr[jmpr] < i + k) jmpr = ngr[jmpr];
+            ans[i] = nums[jmpr];
         }
         return ans;
     }
@@ -14,11 +14,13 @@ class Solution {
     public int[] ngr(int[] arr) {
         Stack<Integer> st = new Stack<>();
         int n = arr.length, ans[] = new int[n];
+        
         for(int i = n - 1; i >= 0; i --) {
-            while(!st.isEmpty() && arr[st.peek()] <= arr[i]) st.pop();
+            while(!st.isEmpty() && arr[st.peek()] <= arr[i])  st.pop();
             ans[i] = st.isEmpty() ? n : st.peek();
             st.push(i);
         }
+        
         return ans;
     }
 }
