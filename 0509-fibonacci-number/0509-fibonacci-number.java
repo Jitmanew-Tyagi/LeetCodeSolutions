8b5cf097f285879d0d201cc62664009b911a072d
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-02-05 14:51:33 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (27.11%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,18 +1,25 @@
 class Solution {
     public int fib(int n) {
         int[] dp = new int[n + 1];
-        // return rec(n);
-        return memo(n, dp);
-    }
-    
-    public int rec(int n) {
-        if(n <= 1) return n;
-        return rec(n - 1) + rec(n - 2);
+        return tab(n, dp);
     }
     
     public int memo(int n, int[] dp) {
         if(n <= 1) return dp[n] = n;
         if(dp[n] != 0) return dp[n];
         return dp[n] = memo(n - 1, dp) + memo(n - 2, dp);
+    }
+    
+    public int tab(int N, int[] dp) {
+        for(int n = 0; n <= N; n ++) {
+            if(n <= 1) {
+                dp[n] = n;
+                continue;
+            }
+            dp[n] = dp[n - 1] + dp[n - 2]; //memo(n - 1, dp) + memo(n - 2, dp);
+            continue;
+        }
+        
+        return dp[N];
     }
 }
