2e1baf37e6b92f491ca8c99d645fe666e57ae76f
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-02-05 15:24:50 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (29.46%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int climbStairs(int n) {
+        // return rec(n);
+        int[] dp = new int[n + 1];
+        return memo(n, dp);
+    }
+    
+    public int rec(int n) {
+        if(n == 0) return 1;
+        int ans = 0;
+        if(n - 1 >= 0) ans += rec(n - 1);
+        if(n - 2 >= 0) ans += rec(n - 2);
+        return ans;
+    }
+    
+    public int memo(int n, int[] dp) {
+        if(n == 0) return dp[n] = 1;
+        if(dp[n] != 0) return dp[n];
+        int ans = 0;
+        if(n - 1 >= 0) ans += memo(n - 1, dp);
+        if(n - 2 >= 0) ans += memo(n - 2, dp);
+        return dp[n] = ans;
+    } 
+}
