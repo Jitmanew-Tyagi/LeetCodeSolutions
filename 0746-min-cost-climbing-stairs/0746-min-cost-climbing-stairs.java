53df3511dbe3b84f524d1ca6dc5d8176a6cdf31c
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-02-06 10:23:01 +0530

    Time: 1 ms (48.57%), Space: 43 MB (56.98%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,25 +1,22 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-        // return Math.min(rec(0, cost.length, cost), rec(1, cost.length, cost));
-        int n = cost.length;
-        int[] dp = new int[n + 2];
+        int n = cost.length, dp[] = new int[n + 2];
+        // return Math.min(rec(0, n, cost), rec(1, n, cost));
         return Math.min(memo(0, n, cost, dp), memo(1, n, cost, dp));
     }
     
     public int rec(int i, int n, int[] cost) {
         if(i >= n) return 0;
-        int pay = cost[i];
-        int oneStep = rec(i + 1, n, cost);
-        int twoStep = rec(i + 2, n, cost);
-        return pay + Math.min(oneStep, twoStep);
-    }
+        int oneStepCost = rec(i + 1, n, cost);
+        int twoStepCost = rec(i + 2, n, cost);
+        return Math.min(oneStepCost, twoStepCost) + cost[i];
+    }  
     
     public int memo(int i, int n, int[] cost, int[] dp) {
         if(i >= n) return dp[i] = 0;
         if(dp[i] != 0) return dp[i];
-        int pay = cost[i];
-        int oneStep = memo(i + 1, n, cost, dp);
-        int twoStep = memo(i + 2, n, cost, dp);
-        return dp[i] = pay + Math.min(oneStep, twoStep);
-    }
+        int oneStepCost = memo(i + 1, n, cost, dp);
+        int twoStepCost = memo(i + 2, n, cost, dp);
+        return dp[i] = Math.min(oneStepCost, twoStepCost) + cost[i];
+    }  
 }
