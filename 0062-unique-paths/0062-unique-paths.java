e8d7f37f4b9f4670eb2d5a6b86543ba41179dc04
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-02-06 15:21:54 +0530

    Time: 1 ms (15.02%), Space: 40.5 MB (35.46%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -2,27 +2,45 @@ class Solution {
     public int uniquePaths(int m, int n) {
         // return rec(0, 0, m -  1, n - 1);
         int[][] dp = new int[m][n];
-        return memo(0, 0, m - 1, n - 1, dp);
+        // return memo(0, 0, m - 1, n - 1, dp);
+        return tab(0, 0, m - 1, n - 1, dp);
     }
     
     public int rec(int sr, int sc, int er, int ec) {
         if(sr == er && sc == ec) return 1;
         
         int ans = 0;
-        if(sr <= er) ans += rec(sr + 1, sc, er, ec);
-        if(sc <= ec) ans += rec(sr, sc + 1, er, ec);
+        if(sr < er) ans += rec(sr + 1, sc, er, ec);
+        if(sc < ec) ans += rec(sr, sc + 1, er, ec);
         
         return ans;
     }
     
     public int memo(int sr, int sc, int er, int ec, int[][] dp) {
-        if(sr > er || sc > ec) return 0;
         if(sr == er && sc == ec) return dp[sr][sc] = 1;
         if(dp[sr][sc] != 0) return dp[sr][sc];
         int ans = 0;
-        ans += memo(sr + 1, sc, er, ec, dp);
-        ans += memo(sr, sc + 1, er, ec, dp);
+        if(sr < er) ans += memo(sr + 1, sc, er, ec, dp);
+        if(sc < ec) ans += memo(sr, sc + 1, er, ec, dp);
         
         return dp[sr][sc] = ans;
     }
+    
+    public int tab(int SR, int SC, int ER, int EC, int[][] dp) {
+        for(int sr = ER; sr >= SR; sr --) {
+            for(int sc = EC; sc >= SC; sc --) {
+                if(sr == ER && sc == EC) {
+                    dp[sr][sc] = 1;
+                    continue;
+                }
+                int ans = 0;
+                if(sr < ER) ans += dp[sr + 1][sc];//memo(sr + 1, sc, er, ec, dp);
+                if(sc < EC) ans += dp[sr][sc + 1];//memo(sr, sc + 1, er, ec, dp);
+                dp[sr][sc] = ans;
+            }
+        }       
+        return dp[SR][SC];
+    }
+    
+    
 }
