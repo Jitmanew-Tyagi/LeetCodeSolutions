79b052beba7e00fe7f5c941047afa32043124999
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-02-06 10:36:15 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (56.98%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -2,7 +2,8 @@ class Solution {
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length, dp[] = new int[n + 2];
         // return Math.min(rec(0, n, cost), rec(1, n, cost));
-        return Math.min(memo(0, n, cost, dp), memo(1, n, cost, dp));
+        // return Math.min(memo(0, n, cost, dp), memo(1, n, cost, dp));
+        return Math.min(tab(0, n, cost, dp), tab(1, n, cost, dp));
     }
     
     public int rec(int i, int n, int[] cost) {
@@ -19,4 +20,17 @@ class Solution {
         int twoStepCost = memo(i + 2, n, cost, dp);
         return dp[i] = Math.min(oneStepCost, twoStepCost) + cost[i];
     }  
+    
+    public int tab(int I, int n, int[] cost, int[] dp) {
+        for(int i = n; i >= I; i --) {
+            if(i >= n) {
+                dp[i] = 0;
+                continue;
+            }
+            int oneStepCost = dp[i + 1];//memo(i + 1, n, cost, dp);
+            int twoStepCost = dp[i + 2];//memo(i + 2, n, cost, dp);
+            dp[i] = Math.min(oneStepCost, twoStepCost) + cost[i];
+        }
+        return dp[I];
+    }
 }
