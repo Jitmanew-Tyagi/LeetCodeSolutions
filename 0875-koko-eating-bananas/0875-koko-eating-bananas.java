893810ad1814f3374c90fa5ec57d4f6cd045444f
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-02-06 21:31:23 +0530

    Time: 26 ms (43.79%), Space: 45.1 MB (34.14%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,26 +1,26 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int min = 1, max = maxOfArray(piles);
+        int s = 1, e = getMax(piles);
         int ans = -1;
-        while(min <= max) {
-            int mid = min + (max - min) / 2;
-            if(isPossible(piles, mid, h)) {
+        while(s <= e) {
+            int mid = s + (e - s) / 2;
+            if(completable(piles, mid, h)) {
                 ans = mid;
-                max = mid - 1;
-            } else min = mid + 1;
+                e = mid - 1;
+            } else s = mid + 1;
         }
         return ans;
     }
     
-    public boolean isPossible(int[] piles, int speed, int hours) {
-        int time = 0;
-        for(int pile : piles) {
-            time += Math.ceil(pile / (speed * 1.0));
+    public boolean completable(int[] bananas, int speed, int allowedHrs) {
+        int hours = 0;;
+        for(int nob : bananas) {
+            hours += Math.ceil(nob / (speed * 1.0));
         }
-        return time <= hours;
+        return hours <= allowedHrs;
     }
     
-    public int maxOfArray(int[] arr) {
+    public int getMax(int[] arr) {
         int max = arr[0];
         for(int ele : arr) if(ele > max) max = ele;
         return max;
