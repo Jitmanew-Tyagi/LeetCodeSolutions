89cafd0053e4d580a959d5388d138bb6c7ba4a29
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-02-07 08:50:05 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (8.89%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,7 +1,7 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        return memo(0, 0, m - 1, n - 1, dp);
+        return tab(0, 0, m - 1, n - 1, dp);
     }
     
     public int memo(int sr, int sc, int er, int ec, int[][] dp) {
@@ -12,4 +12,22 @@ class Solution {
         if(sc < ec) ans += memo(sr, sc + 1, er, ec, dp);
         return dp[sr][sc] = ans;
     }
+    
+    //in which direction should I run the loop such that thee dependencies are always reolved first?
+    public int tab(int SR, int SC, int ER, int EC, int[][] dp) {
+        for(int sr = ER; sr >= SR; sr --) {
+            for(int sc = EC; sc >= SC; sc --) {
+                if(sr == ER && sc == EC) {
+                    dp[sr][sc] = 1;
+                    continue;
+                }
+                int ans = 0;
+                if(sr < ER) ans += dp[sr + 1][sc]; //memo(sr + 1, sc, er, ec, dp);
+                if(sc < EC) ans += dp[sr][sc + 1]; //memo(sr, sc + 1, er, ec, dp);
+                dp[sr][sc] = ans;
+            }
+        }     
+        return dp[SR][SC];
+    }
+    
 }
