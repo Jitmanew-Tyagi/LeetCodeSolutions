cbfda1cda3d05291be3549471c6e7385d8a0b72d
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-02-27 20:39:52 +0530

    Time: 80 ms (19.03%), Space: 44.6 MB (39.02%) - LeetHub

--- 2865-beautiful-towers-i/2865-beautiful-towers-i.java
+++ 2865-beautiful-towers-i/2865-beautiful-towers-i.java
@@ -1,49 +1,20 @@
 class Solution {
     public long maximumSumOfHeights(List<Integer> mh) {
-        int[] nsl = nsl(mh), nsr = nsr(mh);
-        long[] ls = leftSum(mh, nsl), rs = rightSum(mh, nsr);
-        long ans = 0;
-        for(int i = 0; i < mh.size(); i ++) {
-            ans = Math.max(ans, ls[i] + rs[i] - mh.get(i));
-        }
-        return ans;
-    }
-    
-    public int[] nsl(List<Integer> arr) {
-        int n = arr.size(), ans[] = new int[n];
-        Stack<Integer> st = new Stack<>();
-        for(int i = 0; i < n; i ++) {
-            while(!st.isEmpty() && arr.get(st.peek()) >= arr.get(i)) st.pop();
-            ans[i] = st.isEmpty() ? -1 : st.peek();
-            st.push(i);
-        }
-        return ans;
-    }
-    
-    public int[] nsr(List<Integer> arr) {
-        int n = arr.size(), ans[] = new int[n];
-        Stack<Integer> st = new Stack<>();
-        for(int i = n - 1; i >= 0; i --) {
-            while(!st.isEmpty() && arr.get(st.peek()) >= arr.get(i)) st.pop();
-            ans[i] = st.isEmpty() ? -1 : st.peek();
-            st.push(i);
-        }
-        return ans;
-    }
-    
-    public long[] leftSum(List<Integer> arr, int[] nsl) {
-        int n = nsl.length;
-        long ans[] = new long[n];
-        for(int i = 0; i < n; i ++) 
-            ans[i] = nsl[i] == -1 ? (long)arr.get(i) * (i + 1) : (long)(i - nsl[i]) * arr.get(i) + ans[nsl[i]];
-        return ans;
-    }
-    
-    public long[] rightSum(List<Integer> arr, int[] nsr) {
-        int n = nsr.length;
-        long ans[] = new long[n];
-        for(int i = n - 1; i >= 0; i --) {
-            ans[i] = nsr[i] == -1 ? (long)arr.get(i) * (n - i) : (long)(nsr[i] - i) * arr.get(i) + ans[nsr[i]];
+        long ans = -1;
+        for(int b = 0; b < mh.size(); b ++) {
+            int ch = mh.get(b), lm = ch, rm = ch;
+            long tf = ch;
+            for(int i = b - 1; i >= 0; i --) {
+                if(mh.get(i) < lm) lm = mh.get(i);
+                tf += lm;
+            }
+            
+            for(int i = b + 1; i < mh.size(); i ++) {
+                if(mh.get(i) < rm) rm = mh.get(i);
+                tf += rm;
+            }
+            
+            ans = Math.max(tf, ans);
         }
         return ans;
     }
