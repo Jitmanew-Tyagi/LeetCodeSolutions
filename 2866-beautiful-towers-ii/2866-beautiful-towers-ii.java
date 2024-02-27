d569215d35138612456cce29e164e3548b1ec8ee
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-02-27 21:29:33 +0530

    Time: 95 ms (50.91%), Space: 60.6 MB (87.27%) - LeetHub

--- 2866-beautiful-towers-ii/2866-beautiful-towers-ii.java
+++ 2866-beautiful-towers-ii/2866-beautiful-towers-ii.java
@@ -1,49 +1,70 @@
 class Solution {
     public long maximumSumOfHeights(List<Integer> mh) {
-        int[] nsl = nsl(mh), nsr = nsr(mh);
-        long[] ls = leftSum(mh, nsl), rs = rightSum(mh, nsr);
-        long ans = 0;
-        for(int i = 0; i < mh.size(); i ++) {
-            ans = Math.max(ans, ls[i] + rs[i] - mh.get(i));
+        int a[]=new int[mh.size()];
+        for(int i=0;i<mh.size();i++)a[i]=mh.get(i);
+        long[] lbh = lbh(nsl(a), mh), rbh = rbh(nsr(a), mh);
+        long ans = -1;
+        for(int i = 0; i < a.length; i ++) {
+            long tf = lbh[i] + rbh[i] - mh.get(i);
+            ans = Math.max(ans, tf);
         }
         return ans;
     }
     
-    public int[] nsl(List<Integer> arr) {
-        int n = arr.size(), ans[] = new int[n];
+    public int[] nsl(int[] arr) {
         Stack<Integer> st = new Stack<>();
+        int n = arr.length, ans[] = new int[n];
         for(int i = 0; i < n; i ++) {
-            while(!st.isEmpty() && arr.get(st.peek()) >= arr.get(i)) st.pop();
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i]) st.pop();
             ans[i] = st.isEmpty() ? -1 : st.peek();
             st.push(i);
         }
         return ans;
     }
     
-    public int[] nsr(List<Integer> arr) {
-        int n = arr.size(), ans[] = new int[n];
+    public int[] nsr(int[] arr) {
         Stack<Integer> st = new Stack<>();
+        int n = arr.length, ans[] = new int[n];
         for(int i = n - 1; i >= 0; i --) {
-            while(!st.isEmpty() && arr.get(st.peek()) >= arr.get(i)) st.pop();
+            while(!st.isEmpty() && arr[st.peek()] >= arr[i]) st.pop();
             ans[i] = st.isEmpty() ? -1 : st.peek();
             st.push(i);
         }
         return ans;
     }
     
-    public long[] leftSum(List<Integer> arr, int[] nsl) {
+    public long[] lbh(int[] nsl, List<Integer> mh) { //left building heights
         int n = nsl.length;
-        long ans[] = new long[n];
-        for(int i = 0; i < n; i ++) 
-            ans[i] = nsl[i] == -1 ? (long)arr.get(i) * (i + 1) : (long)(i - nsl[i]) * arr.get(i) + ans[nsl[i]];
+        long[] ans = new long[n]; 
+        for(int i = 0; i < n; i ++) {
+            int sl = nsl[i];
+            if(sl == -1) {
+                long tb = i + 1; // total buildings of current height
+                long tf = tb * mh.get(i);
+                ans[i] = tf;
+            } else {
+                long tb = i - sl;
+                long tf = tb * mh.get(i) + ans[sl];
+                ans[i] = tf;
+            }
+        }
         return ans;
     }
     
-    public long[] rightSum(List<Integer> arr, int[] nsr) {
+    public long[] rbh(int[] nsr, List<Integer> mh) { //right building heights
         int n = nsr.length;
-        long ans[] = new long[n];
+        long[] ans = new long[n]; 
         for(int i = n - 1; i >= 0; i --) {
-            ans[i] = nsr[i] == -1 ? (long)arr.get(i) * (n - i) : (long)(nsr[i] - i) * arr.get(i) + ans[nsr[i]];
+            int sr = nsr[i];
+            if(sr == -1) {
+                long tb = n - i; // total buildings of current height
+                long tf = tb * mh.get(i);
+                ans[i] = tf;
+            } else {
+                long tb = sr - i;
+                long tf = tb * mh.get(i) + ans[sr];
+                ans[i] = tf;
+            }
         }
         return ans;
     }
