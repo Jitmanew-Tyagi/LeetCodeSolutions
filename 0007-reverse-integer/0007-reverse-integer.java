65c5da8bb5a5882687ac03425dac265b6a21ccb8
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-04-26 21:00:01 +0530

    Time: 1 ms (83.41%), Space: 40.5 MB (78.03%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,20 +1,12 @@
 class Solution {
-    public int reverse(int x) {
-        boolean isNeg = x < 0;
-        // int n = x > 0 ? x : -x;
-        int n = 0;
-        n = reverseHelper(x);
-        
-        return n;
-    }
-    
-    public int reverseHelper(int n) {
+    public int reverse(int n) {
         long ans = 0;
         while(n != 0) {
             ans = (ans * 10) + (n % 10);
             n /= 10;
         }
-        
         return (ans > Integer.MAX_VALUE || ans < Integer.MIN_VALUE) ? 0 : (int) ans;
     }
+    
+    
 }
