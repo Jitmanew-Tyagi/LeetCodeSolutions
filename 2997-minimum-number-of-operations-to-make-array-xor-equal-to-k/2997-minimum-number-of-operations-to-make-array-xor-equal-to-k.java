af6ff6a751316bbb8816f6d596d8a94ca96398e3
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-04-29 12:19:01 +0530

    Time: 1 ms (100.00%), Space: 58.6 MB (10.88%) - LeetHub

--- 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.java
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.java
@@ -2,7 +2,6 @@ class Solution {
     public int minOperations(int[] nums, int k) {
         int ans = k, count = 0;
         for(int ele : nums) ans ^= ele;
-        for(int i = 0; i < 32; i ++) count += (ans & (1 << i)) > 0 ? 1 : 0;
-        return count;
+        return Integer.bitCount(ans);
     }
 }
