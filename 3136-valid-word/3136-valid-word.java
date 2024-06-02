c9ae7fd504fa5f36650c5ac6bd0f190669d1440e
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-06-02 23:30:58 +0530

    Time: 1 ms (99.32%), Space: 41.5 MB (96.11%) - LeetHub

---
+++ 3136-valid-word/3136-valid-word.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String word) {
+        if(word.length() < 3) return false;
+        boolean v = false, c = false;
+        int noc = 0;
+        for(char ch : word.toCharArray()) {
+            if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+                ch = Character.toLowerCase(ch);
+                if(isVovel(ch)) v = true;
+                else c = true;
+                noc ++;
+            }
+            else if(ch >= '0' && ch <= '9') noc ++;
+            else {
+                return false;
+            }
+        }
+        return c && v && (noc >= 3);
+    }
+    
+    public boolean isVovel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'; 
+    }
+}
