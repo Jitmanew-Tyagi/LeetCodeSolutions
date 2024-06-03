d896bdbdce03c2d4571a6ff32a4c344b951af80d
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-06-03 21:48:11 +0530

    Time: 0 ms (100.00%), Space: 46 MB (23.14%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,34 +1,30 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int focc = firstOccurance(nums, target);
-        int locc = lastOccurance(nums, target);
-        return new int[] {focc, locc};
+        return new int[] {firstOcc(nums, target), lastOcc(nums, target)};
     }
     
-    public int firstOccurance(int[] arr, int target) {
-        int s = 0, e = arr.length - 1;
-        int ans = -1;
+     public int lastOcc(int[] arr, int  target) {
+        int s = 0, e = arr.length - 1, ans = -1;
         while(s <= e) {
-            int mid = s + (e - s) / 2;
+            int mid = (s + e) / 2;
             if(arr[mid] == target) {
                 ans = mid;
-                e = mid - 1;
-            } else if(arr[mid] < target) s = mid + 1;
-            else e = mid - 1;
+                s = mid + 1;
+            } else if(arr[mid] > target) e = mid - 1;
+            else s = mid + 1;
         }
         return ans;
     }
     
-    public int lastOccurance(int[] arr, int target) {
-        int s = 0, e = arr.length - 1;
-        int ans = -1;
+     public static int firstOcc(int[] arr, int  target) {
+        int s = 0, e = arr.length - 1, ans = -1;
         while(s <= e) {
-            int mid = s + (e - s) / 2;
+            int mid = (s + e) / 2;
             if(arr[mid] == target) {
                 ans = mid;
-                s = mid + 1;
-            } else if(arr[mid] < target) s = mid + 1;
-            else e = mid - 1;
+                e = mid - 1;
+            } else if(arr[mid] > target) e = mid - 1;
+            else s = mid + 1;
         }
         return ans;
     }
