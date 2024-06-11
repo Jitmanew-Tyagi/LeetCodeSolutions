30fcf5989e3a3e28189c5a0dd4bb51e7862b7b1f
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-06-11 18:32:52 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (29.83%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,28 +1,30 @@
 class Solution {
     public void rotate(int[][] matrix) {
         transpose(matrix);
-        for(int[] row : matrix) reverse(row);
+        reverseRows(matrix);
     }
     
-     public static void transpose(int[][] arr) {
+    public void transpose(int[][] arr) {
         int n = arr.length;
         for(int i = 0; i < n; i ++) {
-            for(int j = i; j < n; j ++) {
-                int temp = arr[i][j];
-                arr[i][j] = arr[j][i];
-                arr[j][i] = temp;
+            for(int j = i + 1; j < n; j ++) {
+                swap(arr, i, j, j, i);
             }
         }
-    } 
+    }
     
-    public void reverse(int[] arr) {
-        int s = 0, e = arr.length - 1;
-        while(s < e) {
-            int temp = arr[e];
-            arr[e] = arr[s];
-            arr[s] = temp;
-            s ++;
-            e --;
+    public void reverseRows(int[][] arr) {
+        for(int r = 0; r < arr.length; r ++) {
+            int s = 0, e = arr[0].length - 1;
+            while(s <= e) {
+                swap(arr, r, s ++, r, e --);
+            }
         }
+    }
+    
+    public void swap(int[][] arr, int r1, int c1, int r2, int c2) {
+        int temp = arr[r1][c1];
+        arr[r1][c1] = arr[r2][c2];
+        arr[r2][c2] = temp;
     }
 }
