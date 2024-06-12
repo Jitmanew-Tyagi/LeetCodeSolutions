442648c7e8cdbf959d80aea8988cda616f7178bc
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-06-12 18:57:00 +0530

    Time: 0 ms (100.00%), Space: 45.8 MB (57.45%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,31 +1,23 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        return new int[] {firstOcc(nums, target), lastOcc(nums, target)};
+        return new int[] {getOcc(nums, target, true), getOcc(nums, target, false)};
     }
     
-     public int lastOcc(int[] arr, int  target) {
+    
+    public static int getOcc(int[] arr, int x, boolean focc) {
         int s = 0, e = arr.length - 1, ans = -1;
         while(s <= e) {
-            int mid = (s + e) / 2;
-            if(arr[mid] == target) {
+            int mid = s + (e - s) / 2; // (s + e) / 2
+            if(arr[mid] == x) {
                 ans = mid;
-                s = mid + 1;
-            } else if(arr[mid] > target) e = mid - 1;
-            else s = mid + 1;
+                if(focc) e = mid - 1;
+                else s = mid + 1;
+            } else if(arr[mid] < x) s = mid + 1;
+            else e = mid - 1;
         }
         return ans;
     }
     
-     public static int firstOcc(int[] arr, int  target) {
-        int s = 0, e = arr.length - 1, ans = -1;
-        while(s <= e) {
-            int mid = (s + e) / 2;
-            if(arr[mid] == target) {
-                ans = mid;
-                e = mid - 1;
-            } else if(arr[mid] > target) e = mid - 1;
-            else s = mid + 1;
-        }
-        return ans;
-    }
 }
+
+
