81eb71f6f2b60b11bd775b2535d3b5c492d24bb3
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-06-26 14:11:09 +0530

    Time: 6 ms (52.27%), Space: 68.6 MB (32.89%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,39 +10,37 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        
-        ListNode slow = head, fast = head;
-        while(fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        ListNode sh = slow.next;
-        sh = reverseList(sh);
-        return compareLists(head, sh);
-    }
-    
-    
-    public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode current = head;
-
-        while (current != null) {
-            ListNode fwd = current.next;
-            current.next = prev;
-            prev = current;
-            current = fwd;
-        }
-        return prev; // The new head of the reversed list
-    }
-    
-    public boolean compareLists(ListNode list1, ListNode list2) {
-        while (list2 != null) {
-            if (list1.val != list2.val) {
-                return false;
-            }
-            list1 = list1.next;
-            list2 = list2.next;
+        ListNode h1 = head;
+        ListNode mid = getMid(head);
+        ListNode h2 = mid.next;
+        mid.next = null;
+        h2 = reverseList(h2);
+        while(h2 != null) {
+            if(h1.val != h2.val) return false;
+            h1 = h1.next;
+            h2 = h2.next;
         }
         return true;
+    }
+    
+    public ListNode getMid(ListNode head) {
+        ListNode s = head, f = head;
+        while(f.next != null && f.next.next != null) {
+            s = s.next;
+            f = f.next.next;
+        }
+        
+        return s;
+    }
+    
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null, curr = head, next = null;
+        while(curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
     }
 }
