94cc5f62b77765fc883015f501a83597a7870f64
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-06-27 15:27:38 +0530

    Time: 9 ms (17.09%), Space: 42.7 MB (15.53%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -21,11 +21,9 @@ class Solution {
             base.add(root.val + "");
             return base;
         }
-        List<String> left = binaryTreePaths(root.left);
-        List<String> right = binaryTreePaths(root.right);
         List<String> ans = new ArrayList<>();
-        if(left.size() > 0) for(String s : left) ans.add(root.val + "->" + s);
-        if(right.size() > 0) for(String s : right) ans.add(root.val + "->" + s);
+        for(String f : binaryTreePaths(root.left)) ans.add(root.val + "->" + f);
+        for(String f : binaryTreePaths(root.right)) ans.add(root.val + "->" + f);
         return ans;
     }
 }
