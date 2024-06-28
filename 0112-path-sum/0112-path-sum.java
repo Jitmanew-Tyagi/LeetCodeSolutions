7b75126bffb51ca8ec42d336588b1cfa313e4027
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-06-28 15:12:00 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (64.65%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -16,8 +16,8 @@
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root == null) return false;
-        if(root.left == null && root.right == null) return root.val == targetSum;
-        int rSum = targetSum - root.val;
-        return hasPathSum(root.left, rSum) || hasPathSum(root.right, rSum);
+        if(root.left == null && root.right == null && root.val == targetSum) return true;
+        return hasPathSum(root.left, targetSum - root.val) || 
+               hasPathSum(root.right, targetSum - root.val);
     }
 }
