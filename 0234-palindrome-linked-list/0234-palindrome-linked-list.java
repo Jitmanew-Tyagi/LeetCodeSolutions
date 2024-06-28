ebb5832d37d1a1b7bb702303081703c7e79a265e
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-06-28 08:42:40 +0530

    Time: 9 ms (28.06%), Space: 68.3 MB (43.05%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,37 +10,36 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode h1 = head;
         ListNode mid = getMid(head);
-        ListNode h2 = mid.next;
+        System.out.println(mid.val);
+        ListNode nh = mid.next;
         mid.next = null;
-        h2 = reverseList(h2);
-        while(h2 != null) {
-            if(h1.val != h2.val) return false;
-            h1 = h1.next;
-            h2 = h2.next;
+        nh = reverse(nh);
+        while(nh != null) {
+            if(head.val != nh.val) return false;
+            head = head.next;
+            nh = nh.next;
         }
         return true;
     }
     
     public ListNode getMid(ListNode head) {
         ListNode s = head, f = head;
-        while(f.next != null && f.next.next != null) {
+        while( f.next != null && f.next.next != null) {
             s = s.next;
             f = f.next.next;
         }
-        
         return s;
     }
     
-    public ListNode reverseList(ListNode head) {
-        ListNode prev = null, curr = head, next = null;
-        while(curr != null) {
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+    public ListNode reverse(ListNode head) {
+        ListNode p = null, c = head, n = head;
+        while(c != null) {
+            n = c.next;
+            c.next = p;
+            p = c;
+            c = n;
         }
-        return prev;
+        return p;
     }
 }
