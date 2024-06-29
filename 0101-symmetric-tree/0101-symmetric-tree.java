3128ba7c3616a24a838c92003bf8b1aa02f0c696
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-06-29 14:30:31 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (41.16%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -21,6 +21,6 @@ class Solution {
     public boolean helper(TreeNode left, TreeNode right) {
         if(left == null || right == null) return left == null && right == null;
         if(left.val != right.val) return false;
-        return helper(left.right, right.left) && helper(left.left, right.right);
+        return helper(left.left, right.right) && helper(left.right, right.left);
     }
 }
