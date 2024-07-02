563814ac8d4732ff93787dec060b21c3b6a5276f
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-07-02 13:31:48 +0530

    Time: 0 ms (100.00%), Space: 43 MB (74.41%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,12 +15,12 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return helper(root, Long.MIN_VALUE, Long. MAX_VALUE);
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
     
-    public boolean helper(TreeNode root, long min, long max) {
+    public boolean validate(TreeNode root, long min, long max) {
         if(root == null) return true;
         if(root.val <= min || root.val >= max) return false;
-        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+        return validate(root.left, min, root.val) && validate(root.right, root.val, max);
     }
 }
