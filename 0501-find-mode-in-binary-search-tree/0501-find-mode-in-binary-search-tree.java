964e01a0824fa87cd21c3799272746cc3126549d
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-07-02 14:52:39 +0530

    Time: 0 ms (100.00%), Space: 44.8 MB (72.79%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.java
@@ -15,36 +15,27 @@
  */
 class Solution {
     public int[] findMode(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        inorder(root, list);
-        
         List<Integer> arr = new ArrayList<>();
-        int cs = 0, ms = 0, cn = 0;
-        for(int ele : list) {
-            if(ele == cn) cs ++;
-            else {
-                cs = 1;
-                cn = ele;
-            }
-            if(cs > ms) {
-                arr.clear();
-                ms = cs;
-            }
-            
-            if(cs == ms) {
-                arr.add(ele);
-            }
-        }
-        
+        inorder(root, arr);
         int idx = 0, ans[] = new int[arr.size()];
         for(int ele : arr) ans[idx ++] = ele;
         return ans;
     }
-    
+    int cs = 0, ms = 0, cn = 0;
     public void inorder(TreeNode root, List<Integer> ans) {
         if(root == null) return;
-        inorder(root.left, ans);
-        ans.add(root.val);
+        inorder(root.left, ans); 
+        int ele = root.val;
+        if(ele == cn) cs ++;
+        else {
+            cs = 1;
+            cn = ele;
+        }
+        if(cs > ms) {
+            ans.clear();
+            ms = cs;
+        }
+        if(cs == ms) ans.add(ele);
         inorder(root.right, ans);
     }
 }
