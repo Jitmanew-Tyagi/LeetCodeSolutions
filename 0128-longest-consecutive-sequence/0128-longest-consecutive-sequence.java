b81bfbadd61360392d0d90ff7097c479c05b79c2
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-07-04 13:41:52 +0530

    Time: 30 ms (54.73%), Space: 60.5 MB (69.38%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -4,16 +4,18 @@ class Solution {
         for(int ele : nums) hs.add(ele);
         int ans = 0;
         for(int ele : nums) {
-            int lo = ele - 1, hi = ele + 1;
+            int lo = ele - 1;
             while(hs.contains(lo)) {
                 hs.remove(lo);
                 lo --;
             }
             
+            int hi = ele + 1;
             while(hs.contains(hi)) {
                 hs.remove(hi);
                 hi ++;
             }
+            
             int len = hi - lo - 1;
             if(len > ans) ans = len;
         }
