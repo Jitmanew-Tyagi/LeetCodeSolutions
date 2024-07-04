55bdb6bd39f0087da549c622643911ed3e4448ca
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-07-04 14:51:59 +0530

    Time: 14 ms (99.30%), Space: 46.7 MB (96.31%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,20 +1,53 @@
 class MyHashMap {
-    int[] hm;
+    Node[] hm;
     public MyHashMap() {
-        hm = new int[1_000_001];
-        Arrays.fill(hm, -1);
+        hm = new Node[10000];
     }
     
     public void put(int key, int value) {
-        hm[key] = value;
+        int idx = idx(key);
+        if(hm[idx] == null) hm[idx] = new Node(-1, -1);
+        Node prev = find(hm[idx], key);
+        if(prev.next == null) prev.next = new Node(key, value);
+        else prev.next.val = value;
     }
     
     public int get(int key) {
-        return hm[key];
+        int idx = idx(key);
+        if(hm[idx] == null) return -1;
+        Node prev = find(hm[idx], key);
+        return prev.next == null ? -1 : prev.next.val;
     }
     
     public void remove(int key) {
-        hm[key] = -1;
+        int idx = idx(key);
+        if(hm[idx] == null) return;
+        Node prev = find(hm[idx], key);
+        if(prev.next != null) prev.next = prev.next.next;
+    }
+    
+    private Node find(Node bucket, int key) {
+        Node node = bucket, prev = null;
+        while(node != null && node.key != key) {
+            prev = node;
+            node = node.next;
+        }
+        return prev;
+    }
+    
+    public int idx(int key) {
+        return Integer.hashCode(key) % hm.length;
+    }
+    
+    class Node {
+        int key;
+        int val;
+        Node next;
+        
+        Node(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
     }
 }
 
