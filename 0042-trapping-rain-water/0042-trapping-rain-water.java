457ef1480e050f53276a8e1a0134ae97aa51dce7
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-08-29 21:29:56 +0530

    Time: 1 ms (67.10%), Space: 45.4 MB (96.16%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,36 +1,28 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
-        int totalWater = 0;
-        int[] left = getLB(height);
-        int[] right = getRB(height);
-        for(int bar = 0; bar < n; bar ++) {
-            int bh = height[bar];
-            int lb = left[bar], rb = right[bar];
-            int water = Math.min(lb, rb) - bh;
-            if(water > 0) totalWater += water;
+        int[] lb = getLeft(height), rb = getRight(height);
+        int water = 0;
+        for(int i = 0; i < height.length; i ++) {
+            water += Math.max(0, Math.min(lb[i], rb[i]) - height[i]);
         }
-        return totalWater;
+        return water;
+        
     }
     
-    public int[] getLB(int[] bars) {
-        int n = bars.length;
-        int[] ans = new int[n];
-        int lb = 0;
+    public static int[] getLeft(int[] arr) {
+        int blb = 0, n = arr.length, ans[] = new int[n];
         for(int i = 0; i < n; i ++) {
-            ans[i] = lb;
-            if(bars[i] > lb) lb = bars[i];
+            ans[i] = blb;
+            blb = Math.max(blb, arr[i]);
         }
         return ans;
     }
     
-    public int[] getRB(int[] bars) {
-        int n = bars.length;
-        int[] ans = new int[n];
-        int rb = 0;
+    public static int[] getRight(int[] arr) {
+        int blb = 0, n = arr.length, ans[] = new int[n];
         for(int i = n - 1; i >= 0; i --) {
-            ans[i] = rb;
-            if(bars[i] > rb) rb = bars[i];
+            ans[i] = blb;
+            blb = Math.max(blb, arr[i]);
         }
         return ans;
     }
