6e5d907da23563d7e301aef733c0a5ae91aa323c
Jitmanew Tyagi <54436520+Jitmanew-Tyagi@users.noreply.github.com>
2024-08-29 21:32:56 +0530

    Time: 1 ms (67.10%), Space: 45.3 MB (97.28%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -3,7 +3,9 @@ class Solution {
         int[] lb = getLeft(height), rb = getRight(height);
         int water = 0;
         for(int i = 0; i < height.length; i ++) {
-            water += Math.max(0, Math.min(lb[i], rb[i]) - height[i]);
+            int minBoundary = Math.min(lb[i], rb[i]);
+            int waterOnBuilding = minBoundary - height[i];
+            if(waterOnBuilding > 0) water += waterOnBuilding;
         }
         return water;
         
